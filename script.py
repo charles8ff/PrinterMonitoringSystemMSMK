114e844b4676fa8da7702fe3b35d07615d1edd38
Carlos Fisac <charles8ff@gmail.com>
2024-03-18 11:25:57 +0100

    Photos to folder and first attempt to set resolution

--- script.py
+++ script.py
@@ -5,36 +5,50 @@ import time
 from threading import Thread #for the future
 import os
 
-filename = 'photo'
-def take_photo(filename = filename):
+chosen_res = [0, 0]
+
+def set_resolution(cap, width, height):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    
+    
+def take_photo(chosen_res, save_path ='./photos/'):
+    # Check for path 
+    os.makedirs(save_path, exist_ok = True)
+    
     # Initialize the camera
     cap = cv2.VideoCapture(0)
     # Check if the webcam is opened correctly
     if not cap.isOpened():
         raise IOError('Cannot open webcam')
     
+    set_resolution(cap, chosen_res[0], chosen_res[1])
+    
+    ### TIMERS
     cam = time.time()
     camTime = cam - start
-    print('Camera open in : ' + str(camTime)[:5] + ' seconds') # ~90 s in desktop
+    print(f'Camera open in : {str(camTime)[:5]} seconds') # ~90 s in desktop
     
     ret, frame = cap.read()
     now =  datetime.now()
-    filename = str(now.strftime('%Y%m%d_%H%M%S%f.jpg'))
-    print(filename)
+    filename = str(now.strftime('%Y%m%d_%H%M%S.jpg'))
+    full_path = os.path.join(save_path, filename)
+    print(full_path)
     if ret:
-        cv2.imwrite(filename, frame)
-        print(f'Photo taken and saved as {filename}')
+        cv2.imwrite(full_path, frame)
+        print(f'Photo taken and saved as {full_path}')
     else:
         print('Failed to take photo')
     
     # Release the camera
     cap.release()
+
 ### TIMERS   
 start = time.time()
-print('Program started at timestamp: '+ str(start)) # my machine is slow
+print(f'Program started at timestamp: {str(start)}') # my machine is slow
 
-
-take_photo()
+res = [1280, 720]
+take_photo(res)
 
 ### TIMERS 
 print('Success')
